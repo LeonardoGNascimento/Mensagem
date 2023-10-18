c3691ddf8a7ddd051c45e675c840f8d14a8363d2
OMonoBola <hospedagenss@gmail.com>
2023-10-17 22:28:10 -0300

    Instalação styled components e topbar

--- zap2-front/src/pages/chat/index.tsx
+++ zap2-front/src/pages/chat/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import socket from "../../config/socket";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Nav, NavDropdown, Navbar, Row } from "react-bootstrap";
 import toast, { Toaster } from "react-hot-toast";
+import { DivMensagens } from "./components";
 
 interface Props {
   chat: string;
@@ -41,39 +42,51 @@ export function Chat({ chat }: Props) {
   return (
     <>
       <Toaster position="top-right" reverseOrder={false} gutter={8} />
-      <div className="d-flex justify-content-center mb-5 mt-5">
-        <div>
-          <div>
-            {mensagens.map((item: any, index: number) => {
-              return (
-                <p key={index}>
-                  {item.usuario} : {item.mensagem} - {item.time}
-                </p>
-              );
-            })}
-          </div>
-          <div>
-            <textarea
-              value={mensagemAtual}
-              onChange={(item) => setMensagemAtual(item.target.value)}
-              onKeyDown={(event) => {
-                if (event.key !== "Enter" && event.keyCode !== 13) {
-                  return;
-                }
+      <Container>
+        <Navbar bg="primary" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        
+          <Row className="d-flex justify-content-center mb-1">
+            {/* mensagens */}
+            <Col lg={12}>
+              <DivMensagens>
+                {mensagens.map((item: any, index: number) => {
+                  return (
+                    <p key={index}>
+                      {item.usuario} : {item.mensagem} - {item.time}
+                    </p>
+                  );
+                })}
+              </DivMensagens>
+              <div>
+                <textarea
+                  value={mensagemAtual}
+                  onChange={(item) => setMensagemAtual(item.target.value)}
+                  onKeyDown={(event) => {
+                    if (event.key !== "Enter" && event.keyCode !== 13) {
+                      return;
+                    }
 
-                socket.emit("recebido", {
-                  mensagem: mensagemAtual,
-                  time: new Date().toLocaleString(),
-                  chat,
-                  usuario: localStorage.getItem("nome"),
-                });
-                setMensagemAtual("");
-              }}
-            />
-          </div>
-          <a href="/">Voltar</a>
-        </div>
-      </div>
+                    socket.emit("recebido", {
+                      mensagem: mensagemAtual,
+                      time: new Date().toLocaleString(),
+                      chat,
+                      usuario: localStorage.getItem("nome"),
+                    });
+                    setMensagemAtual("");
+                  }}
+                />
+              </div>
+              <a href="/">Voltar</a>
+            </Col>
+          </Row>
+      </Container>
     </>
   );
 }
